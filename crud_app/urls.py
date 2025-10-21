f045e4d3dc47d2f15745ef8ee947c94dadf020c8
SaliMerc <nyawadesaline@gmail.com>
2025-10-21 15:02:03 +0300

    implemented the add function (to allow for teh adding of the blogs'

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', all_blogs, name='all-blogs'),
     
     path('add-blog/', add_blog, name='add-blog'),
-    path('update-blog/', add_blog, name='update-blog'),
-    path('delete-blog/', add_blog, name='delete-blog'),
+    path('update-blog/<int:id>/', update_blog, name='update-blog'),
+    path('delete-blog/<int:id>/', delete_blog, name='delete-blog'),
     
-    path('blog-detail/', add_blog, name='blog-detail'),
+    path('blog-detail/<int:id>/', blog_detail, name='blog-detail'),
 ]
--- crud_app/views.py
+++ crud_app/views.py
@@ -9,16 +9,35 @@ def all_blogs(request):
     blogs = Blog.objects.all().order_by('-created_at')
     return render(request, 'all-blogs.html', {'blogs': blogs})
 
-def blog_detail(request):
-    return render(request, 'blog-detail.html')
+def blog_detail(request, id):
+    blog = get_object_or_404(Blog, id=id)
+    context={'blog': blog}
+    return render(request, 'blog-detail.html', context)
 
 def add_blog(request):
-    return render(request, 'all-blogs.html')
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        image = request.FILES.get('image')
+        author = request.POST.get('author')
+
+        Blog.objects.create(
+            title=title,
+            content=content,
+            image=image,
+            author=author
+        )
+        return redirect('/')
+    return render(request, 'add-blog.html')
 
 def update_blog(request):
+    blog = get_object_or_404(Blog, id=id)
+    context={'blog': blog}
     return render(request, 'update-blog.html')
 
 def delete_blog(request):
+    blog = get_object_or_404(Blog, id=id)
+    context={'blog': blog}
     return render(request, 'delete-blog.html')
 
 
