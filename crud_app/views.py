7dff8735c02ef5effc007ee2e3acd558f6d4b9ea
SaliMerc <nyawadesaline@gmail.com>
2025-10-21 15:07:45 +0300

    implemented the blog update functionality

--- crud_app/views.py
+++ crud_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from .models import Blog
 
 
@@ -27,17 +28,29 @@ def add_blog(request):
             image=image,
             author=author
         )
+        messages.success(request, 'Blog added successfully!')
         return redirect('/')
     return render(request, 'add-blog.html')
 
-def update_blog(request):
+def update_blog(request, id):
     blog = get_object_or_404(Blog, id=id)
+    
+    if request.method == 'POST':
+        blog.title = request.POST.get('title')
+        blog.content = request.POST.get('content')
+        if 'image' in request.FILES:
+            blog.image = request.FILES.get('image')
+        blog.author = request.POST.get('author')
+        blog.save()
+        messages.success(request, 'Blog added successfully!')
+        return redirect('/')
+    
     context={'blog': blog}
-    return render(request, 'update-blog.html')
+    return render(request, 'update-blog.html', context)
 
 def delete_blog(request):
     blog = get_object_or_404(Blog, id=id)
     context={'blog': blog}
-    return render(request, 'delete-blog.html')
+    return render(request, 'delete-blog.html', context)
 
 
