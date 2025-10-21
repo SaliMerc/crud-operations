f783a1587ce2f0912e55448511b4bcd1e34350ed
SaliMerc <nyawadesaline@gmail.com>
2025-10-21 14:54:32 +0300

    created the templates for the crud operatiosn: added them in views render and urls

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -3,4 +3,10 @@ from crud_app.views import *
 
 urlpatterns = [
     path('', all_blogs, name='all-blogs'),
+    
+    path('add-blog/', add_blog, name='add-blog'),
+    path('update-blog/', add_blog, name='update-blog'),
+    path('delete-blog/', add_blog, name='delete-blog'),
+    
+    path('blog-detail/', add_blog, name='blog-detail'),
 ]
--- crud_app/views.py
+++ crud_app/views.py
@@ -4,3 +4,21 @@ from .models import Blog
 
 def index(request):
     return render(request, 'base.html')
+
+def all_blogs(request):
+    blogs = Blog.objects.all().order_by('-created_at')
+    return render(request, 'all-blogs.html', {'blogs': blogs})
+
+def blog_detail(request):
+    return render(request, 'blog-detail.html')
+
+def add_blog(request):
+    return render(request, 'all-blogs.html')
+
+def update_blog(request):
+    return render(request, 'update-blog.html')
+
+def delete_blog(request):
+    return render(request, 'delete-blog.html')
+
+
